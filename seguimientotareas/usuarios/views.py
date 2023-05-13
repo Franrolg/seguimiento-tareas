b80e5993eb12cdc58ee155adee70b7e62bd3296c
Francisco Roldán <francisco.roldang@live.com>
2023-05-13 11:57:15 -0400

    feat: vista de inicio de sesion

--- seguimientotareas/usuarios/views.py
+++ seguimientotareas/usuarios/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
 
-class Usuarios(TemplateView):
-    pass
+from django.shortcuts import render, redirect
+
+def autenticar_usuario(request, usuario, contrasena):
+    user = authenticate(request, username=usuario, password=contrasena)
+    if user: login(request, user)
+    return True if user is not None else False
+
+def iniciar_sesion(request):
+    if request.method == 'POST':
+        messages.success(request, "¡Has iniciado sesión!") if autenticar_usuario(request, request.POST['usuario'], request.POST['contrasena']) else messages.error(request, "¡Error al iniciar sesión!")
+        return redirect('landing')
+    return False
